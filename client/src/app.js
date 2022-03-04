03335a37cb543f5454919ed343007aa39c8cc4b1
kadajett <jeremy.ryan.stover@gmail.com>
2022-03-04 11:47:13 -0500

    initial commit

--- client/src/app.js
+++ client/src/app.js
@@ -2,17 +2,16 @@ import Phaser from "phaser";
 import "./styles/base.css";
 import MageCityImage from "./assets/magecitycut.png";
 import GoblinImage from "./assets/goblin.png";
-import fakeCity from "./FakeCity";
+import Beginnings from "./assets/maps/Beginning/map_beginning.json";
 import { groupBy } from "./utils/array";
 
 var config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
   height: window.innerHeight,
-  player_speed: 100,
+  player_speed: 200,
   physics: {
     default: "arcade",
-
     arcade: {
       gravity: { y: 0, x: 0 },
     },
@@ -50,7 +49,7 @@ function loadMapImages(PhaserContext) {
 }
 
 const loadCharacterSprites = (PhaserContext) => {
-  GM.player = PhaserContext.physics.add.sprite(0, 0, "goblin");
+  GM.player = PhaserContext.physics.add.sprite(1100, 200, "goblin");
   PhaserContext.anims.create({
     key: "left",
     frames: PhaserContext.anims.generateFrameNumbers("goblin", {
@@ -134,62 +133,94 @@ function preload() {
 const setupPlayer = (PhaserContext) => {
   loadCharacterSprites(PhaserContext);
   GM.player.setCollideWorldBounds(true);
+  GM.player.body.setSize(30, 30, 20, 20);
+  window.player = GM.player;
+
+  GM.player.body.width = 20;
+  GM.player.body.height = 20;
+  GM.player.body.offset.x = 10;
+  GM.player.body.offset.y = 40;
 
   PhaserContext.cameras.main.startFollow(GM.player);
 };
 
-function create() {
-  const grasstile = groupBy(
-    fakeCity.layers[0].data,
-    fakeCity.width,
-    fakeCity.height
-  );
-  const stonetile = groupBy(
-    fakeCity.layers[1].data,
-    fakeCity.width,
-    fakeCity.height
-  );
-  const foliagetile = groupBy(
-    fakeCity.layers[2].data,
-    fakeCity.width,
-    fakeCity.height
-  );
+let graphics = null;
 
-  const collisionTile = groupBy(
-    fakeCity.layers[3].data,
-    fakeCity.width,
-    fakeCity.height
-  );
+function create() {
+  graphics = this.add.graphics();
+  console.log("graphics", graphics);
+  const layers = {};
+
+  Beginnings.layers.forEach((layer) => {
+    layers[layer.name] = {
+      data: groupBy(layer.data, Beginnings.width, Beginnings.height),
+      properties: layer.properties,
+    };
+  });
 
   const map = this.make.tilemap({
     key: "cityMap",
     tileWidth: 32,
     tileHeight: 32,
-    data: grasstile,
-    width: fakeCity.width,
-    height: fakeCity.height,
+    width: Beginnings.width,
+    height: Beginnings.height,
   });
 
   const tileset = map.addTilesetImage("city");
 
-  const grasslayer = map.createBlankLayer("grass", tileset, 0, 0);
-  grasslayer.putTilesAt(grasstile, 0, 0, true);
+  const tileLayer = {};
+  let CollisionLayer = {};
 
-  const pathLayer = map.createBlankLayer("path", tileset);
-  pathLayer.putTilesAt(stonetile, 0, 0, true);
+  for (const layer in layers) {
+    console.log(layers[layer]);
+    const layerData = layers[layer].data;
+    const layerProperties = {};
+    if (layers[layer].properties?.length) {
+      layers[layer].properties.forEach((property) => {
+        layerProperties[property.name] = property.value;
+      });
+    }
+
+    tileLayer[layer] = map.createBlankLayer(layer, tileset);
+    tileLayer[layer].putTilesAt(layerData, 0, 0, true);
+    if (layerProperties.Collidable) {
+      tileLayer[layer].setCollisionByExclusion([-1]);
+      CollisionLayer = tileLayer[layer];
+    }
+    if (layerProperties.PlayerLayer) {
+      GM.cursors = this.input.keyboard.createCursorKeys();
+      setupPlayer(this);
+      this.physics.add.collider(GM.player, CollisionLayer);
+      const playerLayerIndex = map.getLayerIndexByName("PlayerLayer");
+      console.log(map.layers[playerLayerIndex]);
+      map.layers[playerLayerIndex].data.forEach((column) => {
+        column.forEach((tile) => {
+          if (tile.index !== -1) {
+            console.log("tile", tile);
+            GM.player.x = tile.pixelX;
+            GM.player.y = tile.pixelY;
+          }
+        });
+      });
+    }
+  }
+  return;
+  pathLayer.putTilesAt(pathTile, 0, 0, true);
 
   const collisionLayer = map.createBlankLayer("collision", tileset);
   collisionLayer.putTilesAt(collisionTile, 0, 0, true);
 
   collisionLayer.setCollisionByExclusion([-1]);
 
-  GM.cursors = this.input.keyboard.createCursorKeys();
   //   GM.player = this.physics.add.sprite(0, 0, GM.player.sprite);
 
   setupPlayer(this);
   this.physics.add.collider(GM.player, collisionLayer);
   const foliageLayer = map.createBlankLayer("foliage", tileset);
   foliageLayer.putTilesAt(foliagetile, 0, 0, true);
+
+  const foliageTopLayer = map.createBlankLayer("foliageTop", tileset);
+  foliageTopLayer.putTilesAt(foliageTop, 0, 0, true);
   //   this.cameras.main.centerOn(GM.player.x, GM.player.y);
 }
 
@@ -198,7 +229,6 @@ let previousState = "idle";
 function update() {
   GM.player.setVelocity(0);
   let currentState = "idle";
-  
 
   if (GM.cursors.left.isDown && !GM.cursors.right.isDown) {
     currentState = "left";
